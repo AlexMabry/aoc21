1a967179a8ee430174a2fe0067a6f2827c58dc29
Alex Mabry <alex.mabry@synapse-wireless.com>
2021-12-06 09:12:03 -0600

    day 5

--- src/d05/amabry_05A.py
+++ src/d05/amabry_05A.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from dataclasses import dataclass
+
 from aocd import models
 from src.utils import parse_data
 
@@ -5,17 +8,33 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2021, day=5)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<x1>[0-9]+),(?P<y1>[0-9]+) -> (?P<x2>[0-9]+),(?P<y2>[0-9]+)'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
 ############################
-# Solve puzzle
-print(input_data)
+@dataclass
+class Point:
+    x: int
+    y: int
 
-answer_to_submit = None
+
+lines = [(Point(int(p.x1), int(p.y1)), Point(int(p.x2), int(p.y2))) for p in input_data]
+
+points = defaultdict(int)
+for p1, p2 in lines:
+    if p1.x == p2.x:  # vert
+        for iy in range(min(p1.y, p2.y), max(p1.y, p2.y) + 1):
+            points[(p1.x, iy)] += 1
+    elif p1.y == p2.y:  # horz
+        for ix in range(min(p1.x, p2.x), max(p1.x, p2.x) + 1):
+            points[(ix, p1.y)] += 1
+
+answer = len([val for val in points.values() if val > 1])
+print(answer)
+
 ############################
 
 # submit answer
-puzzle.answer_a = answer_to_submit
+puzzle.answer_a = answer
--- src/d05/amabry_05B.py
+++ src/d05/amabry_05B.py
@@ -1,21 +1,44 @@
+from collections import defaultdict
+from dataclasses import dataclass
+
 from aocd import models
+
 from src.utils import parse_data
 
 # create puzzle
 puzzle = models.Puzzle(year=2021, day=5)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<x1>[0-9]+),(?P<y1>[0-9]+) -> (?P<x2>[0-9]+),(?P<y2>[0-9]+)'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
-############################
-# Solve puzzle
-print(input_data)
 
-answer_to_submit = None
+############################
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+lines = [(Point(int(p.x1), int(p.y1)), Point(int(p.x2), int(p.y2))) for p in input_data]
+
+points = defaultdict(int)
+for p1, p2 in lines:
+    if p1.x == p2.x:  # vert
+        for iy in range(min(p1.y, p2.y), max(p1.y, p2.y) + 1):
+            points[(p1.x, iy)] += 1
+    elif p1.y == p2.y:  # horz
+        for ix in range(min(p1.x, p2.x), max(p1.x, p2.x) + 1):
+            points[(ix, p1.y)] += 1
+    else:  # diag
+        for i in range(abs(p2.x - p1.x) + 1):
+            points[(p1.x + (i if p2.x > p1.x else -i), p1.y + (i if p2.y > p1.y else -i))] += 1
+
+answer = len([val for val in points.values() if val > 1])
+
 ############################
 
 # submit answer
-puzzle.answer_b = answer_to_submit
+puzzle.answer_b = answer
