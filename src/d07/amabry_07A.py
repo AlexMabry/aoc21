5a82c69392c49cb522d8c03033bea19c6879e6a3
Alex Mabry <alex.mabry@synapse-wireless.com>
2021-12-07 08:53:15 -0600

    day 7

--- src/d07/amabry_07A.py
+++ src/d07/amabry_07A.py
@@ -8,14 +8,23 @@ puzzle = models.Puzzle(year=2021, day=7)
 line_pattern = r'(?P<group_name>.*)'
 
 # format data
-input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
+input_data = parse_data(puzzle.input_data, is_lines=False, is_numbers=False, regex=None)
+crabs = sorted(int(n) for n in input_data.split(','))
 
+import time
+start_time = time.time()
 ############################
-# Solve puzzle
-print(input_data)
+dist = {n: crab_count for n in range(max(crabs)+1) if (crab_count := crabs.count(n)) > 0}
 
-answer_to_submit = None
+min_fuel = 1_000_000
+for this_num in range(max(crabs)+1):
+    this_fuel = 0
+    for n, crab_count in dist.items():
+        this_fuel += abs(this_num - n) * crab_count
+
+    min_fuel = min(this_fuel, min_fuel)
 ############################
+print(time.time() - start_time)
 
 # submit answer
-puzzle.answer_a = answer_to_submit
+puzzle.answer_a = min_fuel
