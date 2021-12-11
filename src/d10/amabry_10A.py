56320963919d4a013193d69bce0b7ffbe218354a
Alex Mabry <alex.mabry@synapse-wireless.com>
2021-12-10 22:58:41 -0600

    day 10

--- src/d10/amabry_10A.py
+++ src/d10/amabry_10A.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from aocd import models
 from src.utils import parse_data
 
@@ -5,17 +7,28 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2021, day=10)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<chunks>.*)'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
 ############################
-# Solve puzzle
-print(input_data)
+CLOSES = {'[': ']', '(': ')', '{': '}', '<': '>'}
+SCORE = {']': 57, ')': 3, '}': 1197, '>': 25137}
 
-answer_to_submit = None
+total = 0
+for line in input_data:
+    q = deque()
+
+    for ic, character in enumerate(line.chunks):
+        if character in '[({<':
+            q.append(character)
+        elif CLOSES[q[-1]] == character:
+            q.pop()
+        else:
+            total += SCORE[character]
+            break
 ############################
 
 # submit answer
-puzzle.answer_a = answer_to_submit
+puzzle.answer_a = total
--- src/d10/amabry_10B.py
+++ src/d10/amabry_10B.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from aocd import models
 from src.utils import parse_data
 
@@ -5,17 +7,39 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2021, day=10)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<chunks>.*)'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
 ############################
-# Solve puzzle
-print(input_data)
+CLOSES = {'[': ']', '(': ')', '{': '}', '<': '>'}
+SCORE = {']': 2, ')': 1, '}': 3, '>': 4}
 
-answer_to_submit = None
+scores = []
+for line in input_data:
+    q = deque()
+    valid = True
+
+    for ic, character in enumerate(line.chunks):
+        if character in '[({<':
+            q.append(character)
+        elif CLOSES[q[-1]] == character:
+            q.pop()
+        else:
+            valid = False
+            break
+
+    if valid:
+        line_score = 0
+        while q:
+            line_score = line_score*5 + SCORE[CLOSES[q[-1]]]
+            q.pop()
+        scores.append(line_score)
+
+sorted_scores = sorted(scores)
+answer = sorted_scores[len(sorted_scores) // 2]
 ############################
 
 # submit answer
-puzzle.answer_b = answer_to_submit
+puzzle.answer_b = answer
