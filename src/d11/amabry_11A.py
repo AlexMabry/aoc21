1d7a8480ff1d58228597b898501f66ca9dab0b15
Alex Mabry <alex.mabry@synapse-wireless.com>
2021-12-11 11:31:56 -0600

    day 11

--- src/d11/amabry_11A.py
+++ src/d11/amabry_11A.py
@@ -1,21 +1,48 @@
+from collections import deque
+
 from aocd import models
+
 from src.utils import parse_data
 
 # create puzzle
 puzzle = models.Puzzle(year=2021, day=11)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<numbers>.*)'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
+import time
+start_time = time.time()
 ############################
-# Solve puzzle
-print(input_data)
+grid = {(ix, iy): int(val) for iy, row in enumerate(input_data) for ix, val in enumerate(row.numbers)}
 
-answer_to_submit = None
+def axis_range(c):
+    return range(max(0, c - 1), min(10, c + 2))
+
+def neighbor_range(p):
+    return ((ix, iy) for ix in axis_range(p[0]) for iy in axis_range(p[1]))
+
+total_flashes = 0
+for step in range(100):
+    grid = {p: val+1 for p, val in grid.items()}
+    to_flash = deque(p for p, val in grid.items() if val == 10)
+
+    already_flashed = set()
+    while to_flash:
+        p = to_flash.pop()
+        already_flashed.add(p)
+
+        for np in neighbor_range(p):
+            grid[np] += 1
+
+        to_flash.extend((np for np in neighbor_range(p) if grid[np] == 10))
+
+    grid = {p: 0 if p in already_flashed else val for p, val in grid.items()}
+    total_flashes += len(already_flashed)
 ############################
+print(time.time() - start_time)
 
 # submit answer
-puzzle.answer_a = answer_to_submit
+puzzle.answer_a = total_flashes
--- src/d11/amabry_11B.py
+++ src/d11/amabry_11B.py
@@ -1,21 +1,57 @@
+from collections import deque
+
 from aocd import models
+
 from src.utils import parse_data
 
 # create puzzle
 puzzle = models.Puzzle(year=2021, day=11)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<numbers>.*)'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
+import time
+start_time = time.time()
 ############################
-# Solve puzzle
-print(input_data)
+grid = {(ix, iy): int(val) for iy, row in enumerate(input_data) for ix, val in enumerate(row.numbers)}
 
-answer_to_submit = None
+def axis_range(c):
+    return range(max(0, c - 1), min(10, c + 2))
+
+def neighbor_range(p):
+    return ((ix, iy) for ix in axis_range(p[0]) for iy in axis_range(p[1]))
+
+for step in range(500):
+    # increase all by one
+    grid = {p: val+1 for p, val in grid.items()}
+
+    # find flashers
+    to_flash = deque(p for p, val in grid.items() if val == 10)
+
+    already_flashed = set()
+    while to_flash:
+        p = to_flash.pop()
+        already_flashed.add(p)
+
+        # increase their neighbors
+        for np in neighbor_range(p):
+            grid[np] += 1
+
+        # add chain flashers
+        to_flash.extend((np for np in neighbor_range(p) if grid[np] == 10))
+
+    # clear all flashers
+    grid = {p: 0 if p in already_flashed else val for p, val in grid.items()}
+
+    # count flashers
+    if len(already_flashed) == 100:
+        answer = step + 1
+        break
 ############################
+print(time.time() - start_time)
 
 # submit answer
-puzzle.answer_b = answer_to_submit
+puzzle.answer_b = answer
