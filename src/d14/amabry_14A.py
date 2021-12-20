0a671e2c87ad57760df8f7ad2a3f69467d54ffeb
Alex Mabry <alex.mabry@synapse-wireless.com>
2021-12-20 13:08:01 -0600

    day 14

--- src/d14/amabry_14A.py
+++ src/d14/amabry_14A.py
@@ -1,3 +1,5 @@
+import itertools
+
 from aocd import models
 from src.utils import parse_data
 
@@ -5,17 +7,24 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2021, day=14)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<template>[A-Z]+$)|((?P<left>[A-Z]+) -> (?P<right>[A-Z]+))'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
 ############################
-# Solve puzzle
-print(input_data)
+template = input_data[0].template
+rules = {row.left: row.right for row in input_data[1:]}
 
-answer_to_submit = None
+for step in range(10):
+    elements = (rules[''.join(pair)] for pair in itertools.pairwise(template))
+    template = ''.join((ct+ce for ct, ce in zip(template, elements))) + template[-1]
+
+counts = {letter: count for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if (count := template.count(letter)) > 0}
+ranked = sorted(counts, key=counts.get, reverse=True)
+answer = counts[ranked[0]] - counts[ranked[-1]]
 ############################
 
+
 # submit answer
-puzzle.answer_a = answer_to_submit
+puzzle.answer_a = answer
--- src/d14/amabry_14B.py
+++ src/d14/amabry_14B.py
@@ -1,3 +1,5 @@
+import itertools
+
 from aocd import models
 from src.utils import parse_data
 
@@ -5,17 +7,39 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2021, day=14)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?P<template>[A-Z]+$)|((?P<left>[A-Z]+) -> (?P<right>[A-Z]+))'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
 ############################
-# Solve puzzle
-print(input_data)
+template = input_data[0].template
+rules = {row.left: row.right for row in input_data[1:]}
 
-answer_to_submit = None
+pair_counts = {pair: 0 for pair in rules.keys()}
+letter_counts = {letter: 0 for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'}
+
+for letter in template:
+    letter_counts[letter] += 1
+
+for pair in itertools.pairwise(template):
+    pair_counts[''.join(pair)] += 1
+
+for step in range(40):
+    new_pair_counts = {pair: 0 for pair in rules.keys()}
+    for pair, count in pair_counts.items():
+        if count:
+            inserted = rules[pair]
+            letter_counts[inserted] += count
+            new_pair_counts[pair[0]+inserted] += count
+            new_pair_counts[inserted+pair[1]] += count
+    pair_counts = {k: v for k, v in new_pair_counts.items()}
+
+letter_counts = {k: v for k, v in letter_counts.items() if v}
+ranked = sorted(letter_counts, key=letter_counts.get, reverse=True)
+answer = letter_counts[ranked[0]] - letter_counts[ranked[-1]]
+print(answer)
 ############################
 
 # submit answer
-puzzle.answer_b = answer_to_submit
+puzzle.answer_b = answer
