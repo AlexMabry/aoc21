96d25226cb13e03b9a024f53f52cf4994c989f1e
Alex Mabry <alex.mabry@synapse-wireless.com>
2021-12-20 13:07:54 -0600

    day 13

--- src/d13/amabry_13A.py
+++ src/d13/amabry_13A.py
@@ -3,19 +3,21 @@ from src.utils import parse_data
 
 # create puzzle
 puzzle = models.Puzzle(year=2021, day=13)
-
-# regex pattern
-line_pattern = r'(?P<group_name>.*)'
-
-# format data
+line_pattern = r'(?:fold along (?P<axis>[xy])=(?P<val>[0-9]+))|(?:(?P<x>[0-9]+),(?P<y>[0-9]+)|)'
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
 ############################
-# Solve puzzle
-print(input_data)
+dots = {(int(row.x), int(row.y)) for row in input_data if row.x}
+folds = [(row.axis, int(row.val)) for row in input_data if row.axis]
 
-answer_to_submit = None
+for axis, val in folds[:1]:
+    if axis == 'x':
+        dots = {(x if x < val else 2*val-x, y) for x, y in dots}
+    else:
+        dots = {(x, y if y < val else 2*val-y) for x, y in dots}
+
+answer = len(dots)
 ############################
 
 # submit answer
-puzzle.answer_a = answer_to_submit
+puzzle.answer_a = answer
--- src/d13/amabry_13B.py
+++ src/d13/amabry_13B.py
@@ -5,17 +5,25 @@ from src.utils import parse_data
 puzzle = models.Puzzle(year=2021, day=13)
 
 # regex pattern
-line_pattern = r'(?P<group_name>.*)'
+line_pattern = r'(?:fold along (?P<axis>[xy])=(?P<val>[0-9]+))|(?:(?P<x>[0-9]+),(?P<y>[0-9]+))'
 
 # format data
 input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
 
 ############################
-# Solve puzzle
-print(input_data)
+dots = {(int(row.x), int(row.y)) for row in input_data if row.x}
+folds = [(row.axis, int(row.val)) for row in input_data if row.axis]
 
-answer_to_submit = None
+for axis, val in folds:
+    if axis == 'x':
+        dots = {(x if x < val else 2*val-x, y) for x, y in dots}
+    else:
+        dots = {(x, y if y < val else 2*val-y) for x, y in dots}
+
+size = (max(p[0] for p in dots), max(p[1] for p in dots))
+for y in range(size[1]+1):
+    print(''.join(['#' if (x, y) in dots else ' ' for x in range(size[0] + 1)]))
 ############################
 
 # submit answer
-puzzle.answer_b = answer_to_submit
+# puzzle.answer_b = answer_to_submit
