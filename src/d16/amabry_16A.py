379a3825381b1d56d96ebb5e0197e18f050311b1
Alex Mabry <alex.mabry@synapse-wireless.com>
2021-12-28 10:34:54 -0600

    day 16

--- src/d16/amabry_16A.py
+++ src/d16/amabry_16A.py
@@ -4,18 +4,71 @@ from src.utils import parse_data
 # create puzzle
 puzzle = models.Puzzle(year=2021, day=16)
 
-# regex pattern
-line_pattern = r'(?P<group_name>.*)'
-
 # format data
-input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
+input_data = parse_data(puzzle.input_data, is_lines=True)
 
 ############################
-# Solve puzzle
-print(input_data)
+payload = ''.join([(bin(int(c, 16))[2:]).zfill(4) for c in input_data[0]])
+print(payload)
+exit()
 
-answer_to_submit = None
+versions = []
+
+
+def packet_header(head):
+    ver = ''.join(payload[head:head+3])
+    tid = ''.join(payload[head+3:head+6])
+    return int(ver, 2), int(tid, 2)
+
+
+def process_packet(head):
+    ver, tid = packet_header(head)
+    versions.append(ver)
+    head += 6
+
+    if tid == 4:
+        val, head = process_literal(head)
+        return head
+    else:
+        lti = payload[head]
+        head += 1
+
+        if lti == '0':
+            total_len = int(''.join(payload[head:head+15]), 2)
+            head += 15
+            packet_end = head + total_len
+
+            while head < packet_end:
+                head = process_packet(head)
+
+            return head
+        else:
+            num_packets = int(''.join(payload[head:head+11]), 2)
+            head += 11
+
+            for i in range(num_packets):
+                head = process_packet(head)
+
+            return head
+
+
+def process_literal(head):
+    val = ''
+    while payload[head] == '1':
+        val += payload[head+1:head+5]
+        head += 5
+
+    val += payload[head+1:head+5]
+    head += 5
+
+    return int(val, 2), head
+
+
+p_head = 0
+process_packet(0)
+
+answer = sum(versions)
 ############################
 
 # submit answer
-puzzle.answer_a = answer_to_submit
+puzzle.answer_a = answer
--- src/d16/amabry_16B.py
+++ src/d16/amabry_16B.py
@@ -1,21 +1,90 @@
+import math
+
 from aocd import models
 from src.utils import parse_data
 
 # create puzzle
 puzzle = models.Puzzle(year=2021, day=16)
 
-# regex pattern
-line_pattern = r'(?P<group_name>.*)'
-
 # format data
-input_data = parse_data(puzzle.input_data, is_lines=True, is_numbers=False, regex=line_pattern)
+input_data = parse_data(puzzle.input_data, is_lines=True)
 
 ############################
-# Solve puzzle
-print(input_data)
+payload = ''.join([(bin(int(c, 16))[2:]).zfill(4) for c in input_data[0]])
 
-answer_to_submit = None
+def packet_header(head):
+    ver = ''.join(payload[head:head+3])
+    tid = ''.join(payload[head+3:head+6])
+    return int(ver, 2), int(tid, 2)
+
+
+def process_packet(head, depth):
+    ver, tid = packet_header(head)
+    head += 6
+
+    if tid == 4:
+        val, head = process_literal(head)
+        print(f"{' ' * depth}LITERAL = {val}")
+        return val, head
+    else:
+        lti = payload[head]
+        head += 1
+
+        values = []
+        if lti == '0':
+            print(f"{' ' * depth}OPERATOR {''.join(payload[head:head+15])}")
+            total_len = int(''.join(payload[head:head+15]), 2)
+            head += 15
+            packet_end = head + total_len
+            print(f"{' ' * depth}OPERATOR {total_len=}")
+
+            while head < packet_end:
+                val, head = process_packet(head, depth+1)
+                values.append(val)
+        else:
+            num_packets = int(''.join(payload[head:head+11]), 2)
+            head += 11
+            print(f"{' ' * depth}OPERATOR {num_packets=}")
+
+            values = []
+            for i in range(num_packets):
+                val, head = process_packet(head, depth+1)
+                values.append(val)
+
+
+        match tid:
+            case 0:
+                return sum(values), head
+            case 1:
+                return math.prod(values), head
+            case 2:
+                return min(values), head
+            case 3:
+                return max(values), head
+            case 5:
+                return 1 if values[0] > values[1] else 0, head
+            case 6:
+                return 1 if values[0] < values[1] else 0, head
+            case 7:
+                return 1 if values[0] == values[1] else 0, head
+
+
+def process_literal(head):
+    val = ''
+    while payload[head] == '1':
+        val += payload[head+1:head+5]
+        head += 5
+
+    val += payload[head+1:head+5]
+    head += 5
+
+    print(head, int(val, 2))
+    return int(val, 2), head
+
+
+answer, _ = process_packet(0, 0)
 ############################
+print(answer)
 
 # submit answer
-puzzle.answer_b = answer_to_submit
+puzzle.answer_b = answer
